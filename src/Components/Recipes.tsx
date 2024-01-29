137c25d026c436e62d2d9b247fe8e1e93a6eb52e
AmandaAshley <amandaashleyjjkk@gmail.com>
2024-01-29 17:04:38 -0300

    Add: Ajustes

--- src/Components/Recipes.tsx
+++ src/Components/Recipes.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
 import Fetcher from '../utils/fetcher';
 
 function Recipes({ recipes }:any) {
-  const { data: search, loading, error } = Fetcher('search');
+  const { data: search, loading, error, isMealsPage } = Fetcher('search');
   const recipesData = recipes && recipes.length > 0 ? recipes : search;
+
+  const navigate = useNavigate();
 
   if (loading) {
     return <p>Loading...</p>;
@@ -21,22 +21,25 @@ function Recipes({ recipes }:any) {
   };
 
   return (
-    <div>
-      <h1>Recipes</h1>
+    <div className="recipesContainer">
+      <h1 className="title">Recipes</h1>
       <div>
         {recipesData?.slice(0, 12).map((recipe: any, index: any) => (
           <button
+            className="recipesCard"
             key={ recipe.idMeal || recipe.idDrink }
             data-testid={ `${index}-recipe-card` }
             onClick={ () => handleCardClick(isMealsPage
               ? recipe.idMeal : recipe.idDrink, isMealsPage) }
           >
             <img
+              className="recipesImg"
               src={ recipe.strMealThumb || recipe.strDrinkThumb }
               alt={ recipe.strMeal || recipe.strDrink }
               data-testid={ `${index}-card-img` }
             />
             <p
+              className="recipesText"
               data-testid={ `${index}-card-name` }
             >
               {recipe.strMeal || recipe.strDrink}
